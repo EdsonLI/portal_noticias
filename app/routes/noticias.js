8e1b3dcc3d5726db934afa228c27d8c8b855239a
unknown <edsonisele@outlook.com>
2017-06-02 19:51:12 -0300

    Continuidade do curso inclusao da conexao com mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,35 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
+    var connection = dbConnection();
+
     app.get('/noticias', function(req, res) {
-        res.render('noticias/noticias');
+
+        connection.query('select * from noticias', function(error, result) {
+            //console.info('passou aqui...');
+            //res.send(result);
+
+            res.render('noticias/noticias', {noticias: result});
+        });
+        //res.render('noticias/noticias');
     });
 }
+/*module.exports = function(app) {
+    app.get('/noticias', function(req, res) {
+      var mysql = require('mysql');
+      var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'edi',
+        password: 'edson',
+        database: 'portal_noticias'
+      });
+      connection.connect();
+
+      connection.query('SELECT * FROM noticias', function(err, rows, fields) {
+        if (err) throw err;
+          console.log(rows[0]);
+        });
+
+      connection.end();
+    });
+}*/
