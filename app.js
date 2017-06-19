0ca70faf6fd838b71ee946fef8bd13100da014e9
EdsonLI <edsonisele@outlook.com>
2017-06-19 20:16:32 -0300

    Edicao do app.js para continuidade do curso NodeJS e MongoDB

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-/*var express = require('express');*/
-//var msg = require('./mod_teste');
-
-/*var app = express();
-
-app.set('view engine', 'ejs');*/
-
-/*app.get('/tecnologia', function(req, res) {
-    res.send('<html><body>Notícias de Tecnologia</body></html>');
-});*/
-
-/*app.get('/tecnologia', function(req, res) {
-    //res.render('secao/tecnologia/');
-    res.render('secao/tecnologia');
-});
-app.get('/', function(req, res) {
-    res.send('<html><body>Portal de Notícias</body></html>');
-});*/
-
 var app = require('./config/server');
 
 var rotaHome = require('./app/routes/home')(app);
@@ -28,22 +9,7 @@ var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 //rotaFormInclusaoNoticia(app);
 
-/* NOVAS ROTAS A PARTIR DA INCLUSAO DAS VIEWS - ADQUIRIDAS PELO DOWNLOAD - AULAS 13 e 14 */
-/*app.get('/', function(req, res) {
-    res.render('home/index');
-});
-
-app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render('admin/form_add_noticia');
-});
-
-app.get('/noticias', function(req, res) {
-    res.render('noticias/noticias');
-});*/
-
 app.listen(3000, function() {
     console.info('Servidor rodando com express na porta 3000');
     //console.info(msg());
 });
-
-/* teste teste teste teste */
