fac6005503ed2632762b01e65a838cadab66c652
EdsonLI <edsonisele@outlook.com>
2017-06-22 19:48:30 -0300

    Atualizacao do projeto com Consign

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 /* Configurações gerais da aplicação */
 
 var express = require('express');
+var consign = require('consign');
 
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', './app/views')
+app.set('views', './app/views');
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
