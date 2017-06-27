bc6cc0d569b4dddde5c1f678ba77269215699c4c
Edson Luis Isele <edsonisele@outlook.com>
2017-06-26 22:09:22 -0300

    Continuacao do curso

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -2,7 +2,7 @@ module.exports = function() {
     this.getNoticias = function(connection, callback) {
         connection.query('select * from noticias', callback);
     };
-    
+
     this.getNoticia = function(connection, callback) {
         connection.query('SELECT * FROM noticias WHERE id_noticia = 2 ', callback);
     }
