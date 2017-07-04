4be742301c2a1c48c9f9c5bc387cfc383951747b
Edson Luis Isele <edsonisele@outlook.com>
2017-07-03 22:50:48 -0300

    Continuidade do curso NodeJS e MongoDB

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,27 @@
 module.exports = function(application) {
     application.get('/formulario_inclusao_noticia', function(req, res) {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao: null});
     });
     application.post('/noticias/salvar', function(req, res) {
         var noticia = req.body;
         //res.send(noticia);
+
+        req.assert('titulo','O título é obrigatório!').notEmpty();
+        req.assert('resumo','O resumo é obrigatório!').notEmpty();
+        req.assert('resumo','O resumo deve conter entre 10 e 100 caracteres!').len(10, 100);
+        req.assert('autor','O nome do autor é obrigatório!').notEmpty();
+        req.assert('data_noticia','A data é obrigatória!').notEmpty();
+        req.assert('data_noticia','O campo data deve conter formato válido para data!').isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia','O texto da notícia é obrigatório!').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros) {
+           //console.info(erros);
+           //res.redirect('/formulario_inclusao_noticia');
+           res.render('admin/form_add_noticia', {validacao: erros});
+           return;
+        }
 
         // conexao
         let connection = application.config.dbConnection();
