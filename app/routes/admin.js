61fb849f19ff2905cccaf484088ff940ae90457e
Edson Luis Isele <edsonisele@outlook.com>
2017-07-05 21:32:01 -0300

    Continuidade do curso

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function(application) {
     application.get('/formulario_inclusao_noticia', function(req, res) {
-        res.render('admin/form_add_noticia', {validacao: null});
+        res.render('admin/form_add_noticia', {validacao:{}, noticia:{}});
     });
     application.post('/noticias/salvar', function(req, res) {
         var noticia = req.body;
@@ -19,7 +19,7 @@ module.exports = function(application) {
         if(erros) {
            //console.info(erros);
            //res.redirect('/formulario_inclusao_noticia');
-           res.render('admin/form_add_noticia', {validacao: erros});
+           res.render('admin/form_add_noticia', {validacao: erros, noticia: noticia});
            return;
         }
 
