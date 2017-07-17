7df41a9f8d6a63d123814144eb4459a2800452f3
Edson Luis Isele <edsonisele@outlook.com>
2017-07-16 22:13:41 -0300

    Adicionado diretorio public e recursos estaticos para o portal

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -3,7 +3,7 @@ function NoticiasDAO(connection) {
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback) {
-    this._connection.query('select * from noticias', callback);
+    this._connection.query('SELECT * FROM noticias', callback);
 }
 
 NoticiasDAO.prototype.getNoticia = function(callback) {
@@ -11,7 +11,11 @@ NoticiasDAO.prototype.getNoticia = function(callback) {
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback) {
-    this._connection.query('INSERT INTO noticias set ? ', noticia, callback);
+    this._connection.query('INSERT INTO noticias SET ? ', noticia, callback);
+}
+
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback) {
+    this._connection.query('SELECT * FROM noticias ORDER BY data_criacao DESC LIMIT 5', callback);
 }
 
 module.exports = function() {
